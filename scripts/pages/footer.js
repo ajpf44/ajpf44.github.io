90f489a23b3f736e01e80d3936a3e9518acae8ec
Alexandre Ferreira <ajpf44@gmail.com>
2025-03-16 17:31:51 -0300

    feat: finish about page

--- scripts/pages/footer.js
+++ scripts/pages/footer.js
@@ -1,11 +1,11 @@
 const footerHTML = `
-        <p>Esse projeto [ <a class="default" href="" target="_blank">repositorio</a> ] foi feito com Vanilla JS</p>        
+        <p>Esse site [ <a class="default" href="" target="_blank">repositorio</a> ] foi feito em Vanilla JS</p>        
         <span>© 2025 Alexandre Ferreira</span>
 `;
 
 /*
     return: void;
-    footerElement: DOM Element
+    footerElement: DOM Elementz
 */
 function renderFooter(footerElement) {
 	if (footerElement != null) {
